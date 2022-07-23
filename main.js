444d1f9cc7964f2d5648dc9bb1c8544156a6388b
Saad Rahman <itssaadrahman@gmail.com>
2022-07-23 20:25:01 +0600

    Added password valdilation

--- main.js
+++ main.js
@@ -21,7 +21,10 @@ function checkInputs() {
   const nameRegex = /^[A-Za-z\s]+$/;
   const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  //first Name
+  const phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  //first Name valdilation
   if (firstNameValue === '') {
     setErrorMessage(firstName, 'Name field cant be blank');
   } else if (!nameRegex.test(firstNameValue)) {
@@ -29,7 +32,7 @@ function checkInputs() {
   } else {
     setSuccesMessage(firstName);
   }
-  //Last Name
+  //Last Name valdilation
   if (lastNameValue === '') {
     setErrorMessage(lastName, 'Name field cant be blank');
   } else if (!nameRegex.test(lastNameValue)) {
@@ -37,13 +40,40 @@ function checkInputs() {
   } else {
     setSuccesMessage(lastName);
   }
-  //Email
+  //Email valdilation
   if (emailValue === ' ') {
     setErrorMessage(email, 'Email cant be blank');
   } else if (!emailRegex.test(emailValue)) {
-    setErrorMessage(email, 'Not a valid email');
+    setErrorMessage(email, 'Enter a valid email');
   } else {
     setSuccesMessage(email);
+  }
+
+  //Phone valdilation
+  if (phoneValue === '') {
+    setErrorMessage(phone, 'Phone cant be blank');
+  } else if (!phoneRegex.test(phoneValue)) {
+    setErrorMessage(phone, 'Enter a valid phone number');
+  } else {
+    setSuccesMessage(phone);
+  }
+
+  //password valdilation
+  if (passwordValue === '') {
+    setErrorMessage(password, 'Password cant be blank');
+  } else if (!passwordRegex.test(passwordValue)) {
+    setErrorMessage(
+      password,
+      'Password must be minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character'
+    );
+  } else setSuccesMessage(password);
+  //confirm password valdilation
+  if (confirmPasswordValue === '') {
+    setErrorMessage(confirmPassword, 'Password cant be blank');
+  } else if (passwordValue === confirmPasswordValue) {
+    setErrorMessage(confirmPassword, 'Password does not match');
+  } else {
+    setSuccesMessage(confirmPassword);
   }
 }
 
